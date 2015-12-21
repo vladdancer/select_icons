c14fd4e250fa29a912d2899dc270467ae0e08fb0
SiliconMind <filip.freelancer@gmail.com>
2015-12-21 17:30:06 +0000

    Initial commit of select icons module.

---
+++ js/ui.selecticons.js
@@ -0,0 +1,41 @@
+(function($) {
+  $.widget('custom.selecticons', $.ui.selectmenu, {
+    _renderItem: function( ul, item ) {
+      var li = $( "<li>", { text: item.label } );
+
+      if ( item.disabled ) {
+        li.addClass('ui-state-disabled');
+      }
+
+      $( "<span>", {
+        style: item.element.attr('data-style'),
+        "class": item.element.attr('data-class')
+      })
+        .appendTo( li );
+
+      return li.appendTo( ul );
+    }
+  });
+
+  $('.selecticons').selecticons({
+    create: function( event, ui ) {
+      // Get classes of currently selected option.
+      var currentClasses = $(this).find('option:selected').attr('data-class');
+      var button = $('#' + this.id + '-button');
+
+      // Create element for current selection's icon.
+      button.prepend('<span class="ui-current-item-icon ' + currentClasses + '"></span>')
+    },
+    select: function( event, ui ) {
+      // Get current option's icon element.
+      var currentIcon = $('#' + this.id + '-button > .ui-current-item-icon');
+
+      // Get selected option's classes.
+      var classes = ui.item.element.attr('data-class');
+
+      // Set new classes for current option's icon element.
+      currentIcon.removeClass();
+      currentIcon.addClass(classes + ' ui-current-item-icon');
+    }
+  });
+})(jQuery);
